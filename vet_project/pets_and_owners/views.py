e61dce4bd61fef7c3aeded55dd897d9fca253c82
BreenLine <breenline@gmail.com>
2022-02-18 01:11:37 +0300

    added base fetatures

---
+++ vet_project/pets_and_owners/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("bu bir sayfa")
+
+
+def about(request):
+    return HttpResponse("about sayfası")
